6888263f60e8fd8f16a4422377476ad264996390
Matt DiPaolo <matt@dldipaolo.com>
2021-09-20 16:42:18 -0400

    server.js changes

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ var server = http.createServer(function (request, response) {
 });
 
 //Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(22);
+server.listen(8080);
 
 
 console.log("Server running at http://127.0.0.1:22/");
