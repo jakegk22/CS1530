b2b98136816c29dca2a08d68402d97842e5bc5a7
Matt DiPaolo <matt@dldipaolo.com>
2021-09-22 17:00:28 -0400

    cleaned up server.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,8 @@
 const express = require('express')
 const path = require("path");
-const fs = require('fs');
-const http = require('http');
 
 const app = express()
 const port = 3000
-
-var indexHTML
-var styleCSS
-var feedCSS
-var profileCSS
-var profileHTML
-var socialfeedHTML 
-
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'..','index.html'));
@@ -20,8 +10,7 @@ app.get('/', (req, res) => {
 app.get('/style.css',(req,res) => {
     res.sendFile(path.join(__dirname,'..','style.css'));
 })
-app.get('')
-  
-  app.listen(port, () => {
+
+app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`)
 })
